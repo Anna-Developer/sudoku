022ccd7d00e46c1bc01a6a5ef326d972a8829f12
Anna <annaange55@gmail.com>
2021-11-03 18:19:58 +0200

    added the game area

--- src/App.js
+++ src/App.js
@@ -4,7 +4,76 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Sudoku</h1>
+        <h1 className="title">Sudoku</h1>
+        <div className="gameArea">
+          <div className="blocks">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+          </div>
+          <div className="blocks">
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+          </div>
+          <div className="blocks">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+          </div>
+          <div className="blocks">
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+          </div>
+          <div className="blocks">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+          </div>
+          <div className="blocks">
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+          </div>
+        </div>
+        <div className="numbers">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </div>
+        <div className="errors">
+          <p>
+            Errors: <span><span>0</span> / 3</span>
+          </p>
+        </div>
       </div>
     </div>
   );
